41d7d9235d2d3d4d4cb59b0817b686c8355fbf6b
trav <trvswgnr@gmail.com>
2018-12-15 01:24:16 -0500

    remove unused functions

--- functions.php
+++ functions.php
@@ -13,8 +13,6 @@ include_function('add-custom-jquery');
 
 include_function('add-scripts');
 
-//include_function('add-custom-post-types');
-
 include_function('add-svg-upload-support');
 
 include_function('remove-head-junk');
