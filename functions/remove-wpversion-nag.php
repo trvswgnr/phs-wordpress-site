854657b45ddae9306d6c69655a6ac22349781b46
trav <trvswgnr@gmail.com>
2018-12-15 01:26:00 -0500

    remove wp version nag in admin

---
+++ functions/remove-wpversion-nag.php
@@ -0,0 +1,7 @@
+<?php
+// no WP version nag in admin
+if (!current_user_can('edit_users')) {
+	add_action('init', function($a) { remove_action('init', 'wp_version_check'); }, 2);
+	add_filter('pre_option_update_core', function($a) { return null; });
+}
+?>
