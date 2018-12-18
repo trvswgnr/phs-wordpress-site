ca94ff111ba93d38ea344a59b5134568d5bac61f
trav <trvswgnr@gmail.com>
2018-12-18 00:23:06 -0500

    add class to navbar when scrolled

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,4 +1,16 @@
-$('.js-menu-toggle').click(function(){
-  $('.mobile-menu').toggleClass('is-active');
-  $('html').toggleClass('nav-open');
+$('.js-menu-toggle').click(function () {
+	$('.mobile-menu').toggleClass('is-active');
+	$('html').toggleClass('nav-open');
+});
+
+var header_position = $(".header").offset().top;
+var height = $('.header').outerHeight();
+
+$(window).scroll(function () {
+	if ($(window).scrollTop() > (height)) {
+		$('header').addClass('is-scrolled');
+	}
+	if ($(window).scrollTop() < (height)) {
+		$('header').removeClass('is-scrolled');
+	}
 });
