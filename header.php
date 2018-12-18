42bee58d4b33f835c9f12e41259e808defda3b82
trav <trvswgnr@gmail.com>
2018-12-18 00:41:10 -0500

    format

--- header.php
+++ header.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?> style="margin-top: 0 !important;">
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="profile" href="http://gmpg.org/xfn/11">
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-<?php wp_head(); ?>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
   <div id="page">
