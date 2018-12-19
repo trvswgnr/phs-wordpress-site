1b53f31b10f08c66785a23edecd61f2e91619ebd
trav <trvswgnr@gmail.com>
2018-12-19 00:25:53 -0500

    fixed height instead of variable

--- src/js/nav.js
+++ src/js/nav.js
@@ -7,10 +7,10 @@ var header_position = $(".header").offset().top;
 var height = $('.header').outerHeight();
 
 $(window).scroll(function () {
-	if ($(window).scrollTop() > (height)) {
+	if ($(window).scrollTop() > (160)) {
 		$('header').addClass('is-scrolled');
 	}
-	if ($(window).scrollTop() < (height)) {
+	if ($(window).scrollTop() < (160)) {
 		$('header').removeClass('is-scrolled');
 	}
 });
