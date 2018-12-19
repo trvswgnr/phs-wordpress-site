08af8dc35a79436d9bebe7971ca3380ab778c4ea
trav <trvswgnr@gmail.com>
2018-12-19 00:26:50 -0500

    acf integration

--- template-parts/home-callout.php
+++ template-parts/home-callout.php
@@ -1,5 +1,9 @@
-<div class="callout section section--image" style="background-image: url(<?php echo get_template_directory_uri() . '/assets/img/bg-home-callout.png'; ?>)">
+<?php
+$callout = get_field('home_callout');
+?>
+
+<div class="callout section section--image" style="background-image: url(<?php if ($callout['image']) { echo $callout['image']; } else { echo get_template_directory_uri() . '/assets/img/bg-home-callout.png'; } ?>)">
 	<div class="container">
-		<h3 class="callout__heading">Officia consequatur facilis, ab distinctio voluptates recusandae quas commodi.</h3>
+		<h3 class="callout__heading"><?php echo $callout['text']; ?></h3>
 	</div>
 </div>
