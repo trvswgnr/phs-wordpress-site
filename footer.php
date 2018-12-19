6b53ef30d94d5fa4c2f30d9dec339779666a11fd
trav <trvswgnr@gmail.com>
2018-12-19 00:23:06 -0500

    format phone number

--- footer.php
+++ footer.php
@@ -1,3 +1,25 @@
+<?php
+function formatPhoneNUmber( $number ){
+  $txt = preg_replace('/[\s\-|\.|\(|\)]/','',$number);
+  $format = '[$1?$1 :][$2?($2):x][$3: ]$4[$5: ]$6[$7? $7:]';
+  if( preg_match('/^(.*)(\d{3})([^\d]*)(\d{3})([^\d]*)(\d{4})([^\d]{0,1}.*)$/', $txt, $matches) ){
+    $result = $format;
+    foreach( $matches AS $k => $v ){
+      $str = preg_match('/\[\$'.$k.'\?(.*?)\:(.*?)\]|\[\$'.$k.'\:(.*?)\]|(\$'.$k.'){1}/', $format, $filterMatch);
+      if( $filterMatch ){
+        $result = str_replace( $filterMatch[0], (!isset($filterMatch[3]) ? (strlen($v) ? str_replace( '$'.$k, $v, $filterMatch[1] ) : $filterMatch[2]) : (strlen($v) ? $v : (isset($filterMatch[4]) ? '' : (isset($filterMatch[3]) ? $filterMatch[3] : '')))), $result );
+      }
+    }
+    return $result;
+  }
+  return $number;
+}
+?>
+
+<?php
+$phone = get_field('info_phone','options');
+?>
+
 <section class="footer">
  <div class="container">
   <div class="footer__logo">
@@ -17,7 +39,7 @@
     endif;
     ?>
     </div>
-    <div class="footer__info"><span><a href="<?php the_field('option_privacy_page','options'); ?>" class="u-color-secondary">Privacy</a></span><span class="u-hidden-sm">&nbsp;&nbsp;|&nbsp;&nbsp;</span><span><a href="mailto:<?php the_field('option_email','options'); ?>" class="u-color-link">Email Us</a></span><span class="u-hidden-sm">&nbsp;&nbsp;|&nbsp;&nbsp;</span><span><a href="tel:<?php the_field('option_phone','options'); ?>" class="u-color-link"><?php the_field('option_phone','options'); ?></a></span></div>
+	<div class="footer__info"><span><a href="/privacy-policy/" class="u-color-secondary">Privacy</a></span><span class="u-hidden-sm">&nbsp;&nbsp;|&nbsp;&nbsp;</span><span><a href="mailto:<?php the_field('info_email','options'); ?>" class="u-color-link">Email Us</a></span><span class="u-hidden-sm">&nbsp;&nbsp;|&nbsp;&nbsp;</span><span><a href="tel:<?php the_field('info_phone','options'); ?>" class="u-color-link"><?php echo formatPhoneNumber($phone); ?></a></span></div>
     <div class="footer__info"><span>© 2018 Premier Home Solutions</span><span class="u-hidden-sm">&nbsp;&nbsp;|&nbsp;&nbsp;</span><span class="website-credit">Website by <a href="http://travisawagner.com" target="_blank" rel="noopener noreferrer">TAW Digital</a></span></div>
   </div>
 </section>
