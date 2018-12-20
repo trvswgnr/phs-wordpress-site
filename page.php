28dcb91486ca5f964e82d9bd996e584b0df6cbd7
trav <trvswgnr@gmail.com>
2018-12-19 19:48:52 -0500

    use custom page heading if available

--- page.php
+++ page.php
@@ -1,11 +1,16 @@
 <?php get_header(); ?>
 
-<div class="container container--sm content">
+<?php
+$page_title = get_field('page_heading');
+$default_title = get_the_title();
+?>
+
+<div class="container container--sm content u-text-center">
   <?php
   if ( have_posts() ) :
     while ( have_posts() ) :
 	?>
-	<h1><?php the_title(); ?></h1>
+	<h1><?php if ($page_title) { echo $page_title; } else { echo $default_title; } ?></h1>
   <?php
 			the_post();
 			the_content();
